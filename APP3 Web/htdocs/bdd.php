8079d17a2de9e196495a458717c3b2009502c8e3
MarreTeint <96467585+MarreTeint@users.noreply.github.com>
2022-03-12 15:56:27 +0100

    patch bdd

--- APP3 Web/htdocs/bdd.php
+++ APP3 Web/htdocs/bdd.php
@@ -1,7 +1,7 @@
 <?php
 
 		try{
-            $bdd = new PDO('mysql:host=sql312.epizy.com;dbname=epiz_30843661_app3;charset=utf8', 'epiz_30843661', 'zMJCjOR2LnGT');
+            $bdd = new PDO('mysql:host=localhost;dbname=app3;charset=utf8', 'root', 'root');
         }
         catch(Exception $e){
             die('Erreur : '.$e->getMessage());
